f0ce8c1feb8adcb90d8bbdd6a583167c3234d7ce
Godfrey <desideryg@gmail.com>
2024-07-15 18:37:15 +0300

    Regular update

--- bus-api/config/auth.php
+++ bus-api/config/auth.php
@@ -16,6 +16,7 @@ return [
     'defaults' => [
         'guard' => 'web',
         'passwords' => 'users',
+
     ],
 
     /*
@@ -40,6 +41,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+            'hash' => false
+        ]
     ],
 
     /*
--- bus-api/routes/api.php
+++ bus-api/routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/test', function(Request $request){
+    return 'Authenticated: My first php API';
 });
+
+
+
+Route::middleware('auth:api')->prefix('v1')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
+});
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+   // return $request->user();
+//});
